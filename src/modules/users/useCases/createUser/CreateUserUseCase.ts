af99c9de351dcb5377148fc41876cabd9832311c
Julia Ramos <juliaramosguedes@gmail.com>
2021-04-11 18:30:56 -0300

    create usecase

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,13 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const emailAlreadyRegistered = this.usersRepository.findByEmail(email);
+
+    if (emailAlreadyRegistered) {
+      throw new Error("Email already registered");
+    }
+
+    return this.usersRepository.create({ name, email });
   }
 }
 
